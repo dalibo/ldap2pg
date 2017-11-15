850410d37f5b29fb091468325425ef309b45389b
Étienne BERSAC <etienne.bersac@dalibo.com>
2017-11-15 16:53:56 +0100

    rpm: Add debug tailf

--- packaging/build_rpm.sh
+++ packaging/build_rpm.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -eux
 
+teardown() {
+    # If not on CI, wait for user interrupt on exit
+    if [ -z "${CI-}" -a $? -gt 0 -a $$ = 1 ] ; then
+        tail -f /dev/null
+    fi
+}
+
+trap teardown EXIT TERM
+
 top_srcdir=$(readlink -m $0/../..)
 cd $top_srcdir
 test -f setup.py
