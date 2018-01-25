0c0f7a90dd5c08bfcc1da30541e4cb129be05698
Étienne BERSAC <etienne.bersac@dalibo.com>
2018-01-25 16:03:57 +0100

    Version 4.3

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ else:
 
 setup(
     name='ldap2pg',
-    version='4.2',
+    version='4.3',
     description='Synchronize PostgreSQL roles and ACLs from LDAP',
     url='https://github.com/dalibo/ldap2pg',
     author='Dalibo',
