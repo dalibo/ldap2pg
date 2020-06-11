62a06e5d5201f4fea082750a81c0ec5f128b2015
Étienne BERSAC <etienne.bersac@dalibo.com>
2020-06-11 08:46:48 +0200

    Log role after their LDAP query

--- ldap2pg/manager.py
+++ ldap2pg/manager.py
@@ -183,6 +183,8 @@ class SyncManager(object):
                     msg = "Role %s redefined with different options." % (
                         role,)
                     raise UserError(msg)
+            else:
+                logger.debug("Want role %s %s.", role, log_source)
             ldaproles[role] = role
 
     def apply_grant_rule(self, rule, ldapacl, vars_, log_source):
@@ -193,7 +195,7 @@ class SyncManager(object):
                     "Ignoring grant on role %s %s. Matches %s.",
                     grant.role, log_source, pattern)
                 continue
-            logger.debug("Found GRANT %s %s.", grant, log_source)
+            logger.debug("Want GRANT %s %s.", grant, log_source)
             ldapacl.add(grant)
 
     def postprocess_acl(self, acl, schemas):
