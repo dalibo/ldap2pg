dc86b4b351cdf9af4d3195738c659d70bdcf4a84
Étienne BERSAC <etienne.bersac@dalibo.com>
2020-06-24 15:11:43 +0200

    rpm: Use dalibo/labs-sdk

--- packaging/build_rpm.sh
+++ packaging/build_rpm.sh
@@ -1,32 +1,22 @@
 #!/bin/bash -eux
 
+top_srcdir=$(readlink -m $0/../..)
+cd "$top_srcdir"
+test -f setup.py
+
+uid_gid=$(stat -c %u:%g "$0")
 teardown() {
 	exit_code=$?
+	chown -R "$uid_gid" "$top_srcdir/dist/"
 	# If not on CI, wait for user interrupt on exit
 	if [ -z "${CI-}" -a $exit_code -gt 0 -a $$ = 1 ] ; then
 		tail -f /dev/null
 	fi
 }
-
 trap teardown EXIT TERM
 
-top_srcdir=$(readlink -m $0/../..)
-cd $top_srcdir
-test -f setup.py
-
-yum_install() {
-    local packages=$*
-    sudo yum install -y $packages
-    rpm --query --queryformat= $packages
-}
-
-# Fasten yum by disabling updates repository
-sudo sed -i '/^\[updates\]/,/^gpgkey=/d' /etc/yum.repos.d/CentOS-Base.repo
-yum_install epel-release
-yum_install python-setuptools
-
 if rpm --query --queryformat= ldap2pg ; then
-    sudo yum remove -y ldap2pg
+    yum remove -y ldap2pg
 fi
 
 rm -rf build/bdist*/rpm
@@ -54,16 +44,21 @@ python setup.py bdist_rpm \
        --requires "${requires}" \
        --spec-only
 
+chown -R "$(id -u):$(id -g)" "$top_srcdir/dist"
 rpmbuild -ba \
+	--define "_builddir %{_topdir}" \
+	--define "_rpmdir %{_topdir}" \
+	--define "_srcrpmdir %{_topdir}" \
+	--define "_sourcedir %{_topdir}" \
+	--define "_specdir %{_topdir}" \
 	--define "_topdir ${top_srcdir}/dist" \
-	--define "_sourcedir ${top_srcdir}/dist" \
 	dist/ldap2pg.spec
 
 rpm="dist/noarch/${fullname}-${release}${rpmdist}.noarch.rpm"
 ln -fs "noarch/$(basename $rpm)" dist/ldap2pg-last.rpm
 
 # Test it
-sudo yum install -y "$rpm"
+yum install -y "$rpm"
 cd /
 test -x /usr/bin/ldap2pg
 python -c 'import ldap2pg'
