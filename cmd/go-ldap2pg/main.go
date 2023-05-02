ad9a34eb4b0a8cc569e808237771126a6bdbdd07
Étienne BERSAC <etienne.bersac@dalibo.com>
2023-05-02 14:47:40 +0200

    go: Refactor entrypoint func stack

--- cmd/go-ldap2pg/main.go
+++ cmd/go-ldap2pg/main.go
@@ -18,17 +18,6 @@ import (
 )
 
 func main() {
-	// Split error management from business logic. This allows defer to
-	// apply before calling os.Exit. Also, deduplicate fatal error logging.
-	// Simply return an error and main will handle this case.
-	err := ldap2pg()
-	if err != nil {
-		slog.Error("Fatal error.", "err", err)
-		os.Exit(1)
-	}
-}
-
-func ldap2pg() (err error) {
 	// Bootstrap logging first to log in setup.
 	config.SetLoggingHandler(slog.LevelInfo, isatty.IsTerminal(os.Stderr.Fd()))
 	SetupConfig()
@@ -39,11 +28,14 @@ func ldap2pg() (err error) {
 		showVersion()
 		return
 	}
-	err = sync()
-	return
+	err := ldap2pg()
+	if err != nil {
+		slog.Error("Fatal error.", "err", err)
+		os.Exit(1)
+	}
 }
 
-func sync() (err error) {
+func ldap2pg() (err error) {
 	start := time.Now()
 
 	controller, err := UnmarshalController()
