f2a01a4c68db2c3f544ac0cecaad89977917d3f6
Étienne BERSAC <etienne.bersac@dalibo.com>
2023-06-20 11:20:28 +0200

    go: Build GRANT and REVOKE queries

--- internal/privilege/wellknown.go
+++ internal/privilege/wellknown.go
@@ -15,23 +15,32 @@ var (
 func init() {
 	Map = make(map[string]Privilege)
 
-	register(
-		"instance",
-		"DATABASE",
-		inspectDatabase,
-		`GRANT %s ON DATABASE %%s TO %%s;`,
-		`REVOKE %s ON DATABASE %%s FROM %%s;`,
-	)
-	register(
-		"instance",
-		"LANGUAGE",
-		inspectLanguage,
-		`GRANT %s ON LANGUAGE %%s TO %%s;`,
-		`REVOKE %s ON LANGUAGE %%s FROM %%s;`,
-	)
+	register("instance", "DATABASE", inspectDatabase)
+	register("instance", "LANGUAGE", inspectLanguage)
 }
 
-func register(scope, object, inspect, grant, revoke string) {
+// queries are grant and revoke queries in order.
+func register(scope, object, inspect string, queries ...string) {
+	var grant, revoke string
+
+	if 0 < len(queries) {
+		grant = queries[0]
+		queries = queries[1:]
+	} else {
+		grant = `GRANT %s ON ` + object + ` %%s TO %%s;`
+	}
+
+	if 0 < len(queries) {
+		revoke = queries[0]
+		queries = queries[1:]
+	} else {
+		revoke = `REVOKE %s ON ` + object + ` %%s FROM %%s;`
+	}
+
+	if 0 < len(queries) {
+		panic("too many queries")
+	}
+
 	Map[object] = Privilege{
 		Scope:   scope,
 		Object:  object,
