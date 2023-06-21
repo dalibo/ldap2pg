8eb435eed634084c0cc039012a77b7bf58a1f028
Étienne BERSAC <etienne.bersac@dalibo.com>
2023-06-21 16:38:54 +0200

    go: Replace Grantor by Owner
    Holding grantor is a design error.

--- internal/privilege/grant.go
+++ internal/privilege/grant.go
@@ -10,13 +10,18 @@ import (
 
 // Grant holds privilege informations from Postgres inspection or Grant rule.
 //
-// Not to confuse with Privilege. A Grant references a privilege, an object and
-// roles. It's more like aclitem object in PostgreSQL.
+// Not to confuse with Privilege. A Grant references an object, a role and a
+// privilege via the Target field. It's somewhat like aclitem object in
+// PostgreSQL.
+//
+// When Owner is non-zero, the grant represent a default privilege grant. The
+// meansing of Object field change to hold the privilege class : TABLES,
+// SEQUENCES, etc. instead of the name of an object.
 type Grant struct {
-	Target   string // Name of the target object: DATABASE, TABLES, etc.
-	Grantor  string
-	Grantee  string // "" means default privilege for Grantor.
-	Type     string
+	Target   string // Name of the referenced privilege object: DATABASE, TABLES, etc.
+	Owner    string // For default privilege. Empty otherwise.
+	Grantee  string
+	Type     string // Privilege type (USAGE, SELECT, etc.)
 	Database string // "" for instance grant.
 	Schema   string // "" for database grant.
 	Object   string // "" for both schema and database grants.
@@ -24,8 +29,12 @@ type Grant struct {
 }
 
 func RowTo(row pgx.CollectableRow) (g Grant, err error) {
-	err = row.Scan(&g.Grantor, &g.Grantee, &g.Type, &g.Database, &g.Schema, &g.Object, &g.Partial)
+	err = row.Scan(&g.Owner, &g.Grantee, &g.Type, &g.Database, &g.Schema, &g.Object, &g.Partial)
 	return
+}
+
+func (g Grant) IsDefault() bool {
+	return "" != g.Owner
 }
 
 // Expand wanted grants.
@@ -44,9 +53,6 @@ func (g Grant) Expand(databases postgres.DBMap) (out []Grant) {
 // This way grants from wanted state and from inspect are comparables.
 func (g *Grant) Normalize() {
 	p := g.Privilege()
-
-	// For now, drop grantor, to remove Grantor from comparison with wanted grant.
-	g.Grantor = ""
 
 	switch p.Scope {
 	case "instance":
@@ -77,37 +83,36 @@ func (g Grant) String() string {
 	if g.Partial {
 		b.WriteString("PARTIAL ")
 	}
-	if "" == g.Grantee {
-		b.WriteString("DEFAULT ")
+	if g.IsDefault() {
+		b.WriteString("DEFAULT FOR ")
+		b.WriteString(g.Owner)
+		b.WriteByte(' ')
 	}
 	b.WriteString(g.Type)
 	b.WriteString(" ON ")
 	b.WriteString(g.Target)
-	b.WriteByte(' ')
-
-	o := strings.Builder{}
-	o.WriteString(g.Database)
-	if "" != g.Schema {
-		if o.Len() > 0 {
-			o.WriteByte('.')
+	if !g.IsDefault() {
+		b.WriteByte(' ')
+		o := strings.Builder{}
+		o.WriteString(g.Database)
+		if "" != g.Schema {
+			if o.Len() > 0 {
+				o.WriteByte('.')
+			}
+			o.WriteString(g.Schema)
 		}
-		o.WriteString(g.Schema)
-	}
-	if "" != g.Object {
-		if o.Len() > 0 {
-			o.WriteByte('.')
+		if "" != g.Object {
+			if o.Len() > 0 {
+				o.WriteByte('.')
+			}
+			o.WriteString(g.Object)
 		}
-		o.WriteString(g.Object)
+		b.WriteString(o.String())
 	}
 
-	b.WriteString(o.String())
 	if "" != g.Grantee {
 		b.WriteString(" TO ")
 		b.WriteString(g.Grantee)
-	}
-	if "" != g.Grantor {
-		b.WriteString(" GRANTED BY ")
-		b.WriteString(g.Grantor)
 	}
 
 	return b.String()
