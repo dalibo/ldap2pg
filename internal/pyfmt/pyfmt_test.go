73a7cf24dfb8e321aa6577d3a5312a5fcf533464
Étienne BERSAC <etienne.bersac@dalibo.com>
2023-06-26 11:31:33 +0200

    go: Don't setup logging for pyfmt tests

--- internal/pyfmt/pyfmt_test.go
+++ internal/pyfmt/pyfmt_test.go
@@ -1,23 +1,10 @@
 package pyfmt_test
 
 import (
-	"testing"
-
-	"github.com/dalibo/ldap2pg/internal"
 	"github.com/stretchr/testify/suite"
-	"golang.org/x/exp/slog"
 )
 
 // Declare a global suite for the package.
 type Suite struct {
 	suite.Suite
-}
-
-func Test(t *testing.T) {
-	if testing.Verbose() {
-		internal.SetLoggingHandler(slog.LevelDebug, false)
-	} else {
-		internal.SetLoggingHandler(slog.LevelWarn, false)
-	}
-	suite.Run(t, new(Suite))
 }
