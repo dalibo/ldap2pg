e8b47b66de53e918c3a6d3bd44faed4b4fa5eb21
Étienne BERSAC <etienne.bersac@dalibo.com>
2023-07-26 14:04:09 +0200

    go: Support Postgres 9.3

--- internal/inspect/sql/session.sql
+++ internal/inspect/sql/session.sql
@@ -9,10 +9,10 @@ postgres AS (
 	SELECT
 		substring(version() from '^[^ ]+ [^ ]+') AS server_version,
 		svn.setting::BIGINT AS server_version_num,
-		cn.setting AS cluster_name,
+		COALESCE(cn.setting, '') AS cluster_name,
 		current_database() AS current_database
 	FROM pg_catalog.pg_settings AS svn
-	JOIN pg_catalog.pg_settings AS cn
+	LEFT OUTER JOIN pg_catalog.pg_settings AS cn
 	  ON "cn"."name" = 'cluster_name'
 	WHERE "svn"."name" = 'server_version_num'
 )
