b8f708b0fecbd6d09da0e41cf34a288d8a66bf41
Étienne BERSAC <etienne.bersac@dalibo.com>
2023-07-28 16:28:45 +0200

    Exclude superusers when running as non superuser

--- internal/inspect/sql/roles.sql
+++ internal/inspect/sql/roles.sql
@@ -1,3 +1,6 @@
+WITH me AS (
+ SELECT * FROM pg_catalog.pg_roles WHERE rolname = CURRENT_USER
+)
 SELECT
   rol.rolname,
   -- Encapsulate columns variation in a sub-row.
@@ -5,9 +8,10 @@ SELECT
   COALESCE(pg_catalog.shobj_description(rol.oid, 'pg_authid'), '') as comment,
   array_remove(array_agg(parents.rolname), NULL) AS parents,
 	rol.rolconfig AS config
-FROM
-  pg_catalog.pg_roles AS rol
+FROM me
+CROSS JOIN pg_catalog.pg_roles AS rol
 LEFT JOIN pg_catalog.pg_auth_members AS membership ON membership.member = rol.oid
 LEFT JOIN pg_catalog.pg_roles AS parents ON parents.oid = membership.roleid
+WHERE NOT (rol.rolsuper AND NOT me.rolsuper)
 GROUP BY 1, 2, 3, 5
 ORDER BY 1;
