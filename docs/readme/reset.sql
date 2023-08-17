cd7c1e93e2602a0ed55d42fa21bd590f33225466
Étienne BERSAC <etienne.bersac@dalibo.com>
2023-08-17 15:38:59 +0200

    Purge old scenarios

--- docs/readme/reset.sql
+++ docs/readme/reset.sql
@@ -1,6 +1,4 @@
 -- Execute this one before executing ldap2pg.minimal.yml to have a few changes.
-DROP DATABASE "appdb";
-DROP DATABASE "olddb";
 DROP ROLE "domitille";
 CREATE ROLE "oscar" IN ROLE "ldap_roles";
 ALTER ROLE "albert" WITH NOLOGIN;
