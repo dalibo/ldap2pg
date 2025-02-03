af8081ea143e71bcaede72c2658b853d6d085393
Étienne BERSAC <etienne.bersac@dalibo.com>
2025-02-03 17:19:05 +0100

    Exit only in main function
    Ensure deferred are always called.

---
+++ cmd/ldap2pg/exit.go
@@ -0,0 +1,21 @@
+package main
+
+import "os"
+
+// Custom error handling exit code.
+//
+// os.Exit() bypasses deferred functions.
+// This error allows passing exit code as an error
+// to execute deferred functions before exiting in caller.
+type errorCode struct {
+	code    int
+	message string
+}
+
+func (err errorCode) Error() string {
+	return err.message
+}
+
+func (err errorCode) Exit() {
+	os.Exit(err.code)
+}
