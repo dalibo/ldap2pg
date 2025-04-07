64db42e994a29441b2bc3c05a9bee432f42a34d6
Étienne BERSAC <etienne.bersac@dalibo.com>
2025-04-07 17:17:49 +0200

    Read version from standard build info

--- internal/cmd/version.go
+++ internal/cmd/version.go
@@ -1,12 +1,56 @@
 package cmd
 
-var (
-	commit  = "<none>"
-	version = "snapshot"
+import (
+	"fmt"
+	"runtime"
+	"runtime/debug"
+	"slices"
 )
 
-func init() {
-	if 8 < len(commit) {
-		commit = commit[:8]
+var (
+	commit   string
+	versions = make(map[string]string)
+	mainDeps = []string{
+		"github.com/jackc/pgx/v5",
+		"github.com/go-ldap/ldap/v3",
+		"gopkg.in/yaml.v3",
 	}
+)
+
+func version() string {
+	return versions["github.com/dalibo/ldap2pg"]
+}
+
+func init() {
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		panic("Failed to read build information.")
+	}
+	for _, mod := range bi.Deps {
+		if slices.Contains(mainDeps, mod.Path) {
+			versions[mod.Path] = mod.Version
+		}
+		if len(versions) >= len(mainDeps) {
+			break
+		}
+	}
+
+	versions[bi.Main.Path] = bi.Main.Version
+
+	for i := range bi.Settings {
+		if bi.Settings[i].Key == "vcs.revision" {
+			commit = bi.Settings[i].Value
+			break
+		}
+	}
+}
+
+func showVersion() {
+	fmt.Printf("ldap2pg %s\n", version())
+
+	for _, path := range mainDeps {
+		fmt.Printf("%s %s\n", path, versions[path])
+	}
+
+	fmt.Printf("%s %s %s\n", runtime.Version(), runtime.GOOS, runtime.GOARCH)
 }
