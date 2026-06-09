dalibo/ldap2pg

commit 7150b20be7ec73fc8d20b49cfd032059f73458d5

Check membership loop

internal/role/map.go
@@ -8,37 +8,40 @@ import (
 
 type Map map[string]Role
 
+func (m Map) Check() error {
+	for _, role := range m {
+		err := role.Check(m, nil)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func (m Map) Flatten() []string {
 	var names []string
 	seen := mapset.NewSet[string]()
 	for _, role := range m {
-		for name := range m.flattenRole(role, &seen) {
-			names = append(names, name)
-		}
+		names = append(names, m.flattenRole(role, &seen)...)
 	}
 	return names
 }
 
-func (m Map) flattenRole(r Role, seen *mapset.Set[string]) (ch chan string) {
-	ch = make(chan string)
-	go func() {
-		defer close(ch)
-		if (*seen).Contains(r.Name) {
-			return
+func (m Map) flattenRole(r Role, seen *mapset.Set[string]) []string {
+	var names []string
+	if (*seen).Contains(r.Name) {
+		return names
+	}
+	for _, membership := range r.Parents {
+		parent, ok := m[membership.Name]
+		if !ok {
+			slog.Debug("Role inherits unmanaged parent.", "role", r.Name, "parent", membership.Name)
+			continue
 		}
-		for _, membership := range r.Parents {
-			parent, ok := m[membership.Name]
-			if !ok {
-				slog.Debug("Role herits unmanaged parent.", "role", r.Name, "parent", membership.Name)
-				continue
-			}
-			for deepName := range m.flattenRole(parent, seen) {
-				ch <- deepName
-			}
-		}
+		names = append(names, m.flattenRole(parent, seen)...)
 
 		(*seen).Add(r.Name)
-		ch <- r.Name
-	}()
-	return
+	}
+	names = append(names, r.Name)
+	return names
 }

internal/wanted/map.go
@@ -116,6 +116,11 @@ func (m Rules) Run(blacklist lists.Blacklist) (roles role.Map, grants map[string
 		}
 	}
 
+	err = roles.Check()
+	if err != nil {
+		errList = append(errList, err)
+	}
+
 	if 0 < len(errList) {
 		err = errors.Join(errList...)
 	}

internal/role/membership_test.go
@@ -27,3 +27,18 @@ func TestMissingParents(t *testing.T) {
 	missing := current.MissingParents(wanted.Parents)
 	r.Len(missing, 1)
 }
+
+func TestLoop(t *testing.T) {
+	r := require.New(t)
+
+	toto := role.Role{
+		Name: "toto",
+		Parents: []role.Membership{
+			{Name: "toto"},
+		},
+	}
+	m := make(role.Map)
+	m[toto.Name] = toto
+	err := m.Check()
+	r.Error(err)
+}

internal/role/role.go
@@ -1,6 +1,9 @@
 package role
 
 import (
+	"fmt"
+	"slices"
+
 	"github.com/dalibo/ldap2pg/internal/postgres"
 	mapset "github.com/deckarep/golang-set/v2"
 	"github.com/jackc/pgx/v5"
@@ -46,6 +49,26 @@ func RowTo(row pgx.CollectableRow) (r Role, err error) {
 	r.Options.LoadRow(variableRow.([]any))
 	(*r.Config).Parse(config)
 	return
+}
+
+func (r Role) Check(m Map, genealogy []string) error {
+	if slices.Contains(genealogy, r.Name) {
+		return fmt.Errorf("membership loop: %s is member of %s", genealogy[0], r.Name)
+	}
+
+	genealogy = append([]string{r.Name}, genealogy...)
+	for _, ms := range r.Parents {
+		p, ok := m[ms.Name]
+		if !ok {
+			continue
+		}
+		err := p.Check(m, genealogy)
+		if err != nil {
+			return fmt.Errorf("%s: %w", r.Name, err)
+		}
+	}
+
+	return nil
 }
 
 func (r *Role) String() string {

commit 5c805c5241a0140cf600e91637f836e37e9a9c00

Fix duplicate CREATE ROLE
Bug introduced by 7150b20.

internal/role/map.go
@@ -39,9 +39,9 @@ func (m Map) flattenRole(r Role, seen *mapset.Set[string]) []string {
 			continue
 		}
 		names = append(names, m.flattenRole(parent, seen)...)
-
-		(*seen).Add(r.Name)
 	}
 	names = append(names, r.Name)
+	(*seen).Add(r.Name)
+
 	return names
 }

internal/role/map_test.go
@@ -1,0 +1,20 @@
+package role_test
+
+import (
+	"testing"
+
+	"github.com/dalibo/ldap2pg/v6/internal/role"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFlatten(t *testing.T) {
+	r := require.New(t)
+
+	m := make(role.Map)
+	m["group"] = role.Role{Name: "group"}
+	m["member"] = role.Role{Name: "member", Parents: []role.Membership{{Name: "group"}}}
+	names := m.Flatten()
+	r.Len(names, 2)
+	r.Equal("group", names[0])
+	r.Equal("member", names[1])
+}